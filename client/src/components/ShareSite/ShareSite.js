eac0aa6ef0f1d662ce8a81dd83ffc553cf3f41ea
Luay Abbas <abbasluay1981@gmail.com>
2022-12-07 14:12:39 +0100

    share-site

--- client/src/components/ShareSite/ShareSite.js
+++ client/src/components/ShareSite/ShareSite.js
@@ -3,6 +3,10 @@ import React from 'react'
 function ShareSite() {
   return (
     <div className='chat-site'>
+      <section className='section-one'></section>
+      <section className='section-two'></section>
+      <section className='section-three'></section>
+      <section className='section-four'></section>
         <footer>
           <div className="footer-container">
             
