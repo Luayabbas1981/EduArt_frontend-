0ba7f2055275aaa23b847a4eb114a6d41ddce79b
Luay Abbas <abbasluay1981@gmail.com>
2022-12-09 23:56:35 +0100

    share-site

--- client/src/components/ShareSite/ShareSite.js
+++ client/src/components/ShareSite/ShareSite.js
@@ -11,10 +11,17 @@ const [emoji,setEmoji]=useState("")
 const [messageTools,setMessageTools]=useState(true)
 const [emojiToll,setEmojiTool]=useState(false)
 const [textToll,setTextTool]=useState(false)
+const [size,setSize]=useState("")
+const [color,setColor] = useState("")
+const [scale,setScale]= useState(false)
 
 useEffect(()=>{
   document.querySelector(".share-site-textarea").focus()
 })
+
+function minimizeHandler(){
+  setScale(!scale)
+}
 
   function inputHandler(e){
     setInput( e.target.value)
@@ -40,6 +47,19 @@ function textToolHandler(){
   setTextTool(true)
   
 }
+
+
+  function colorHandler(e){
+    setColor(e.target.value)
+  }
+
+console.log(color)
+
+function sizeHandler(e){
+  setSize(e.target.id)
+
+}
+console.log(size)
 function goBackHandler(){
   setMessageTools(true)
 }
@@ -73,35 +93,38 @@ async function sendMessageHandler (e){
     <div className='chat-site'>
       <main className="share-site-main">
       <section className='section-one'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur vel repellat laboriosam. Ab laborum, illum ad iusto facilis quaerat sit praesentium architecto aliquid, facilis aut vitae odio sequi nemo accusantium ea repellendus quibusdam autem deleniti error unde?</section>
-      <section className='section-two'></section>
+      <section className='section-two'>orem ipsum dolor sit amet consectetur adip</section>
       <section className='section-three'></section>
       <section className='section-four'></section>
       </main>
-        <footer>
-          <div className="share-site-footer-container">
-            <div className="textarea-container">
-            <textarea className="share-site-textarea" onChange={inputHandler} value={input} />
+        <footer className="share-site-footer">
+          <div className= "share-site-footer-container"  style={{scale:`${scale? ".8":".4"}`}}> 
+            <div className="textarea-container" style={{scale:`${scale? "1":"0"}`}}>
+            <textarea className="share-site-textarea" onChange={inputHandler} value={input} style={{fontSize:size,color:color }}/>
             </div>
             <div className="share-site-send-message" onClick={sendMessageHandler}>
             <i className="fa-solid fa-paper-plane"></i>
             </div>
+            
             <div className="message-tool-container">
               {messageTools? <>
               
-                <div className="emoji-tool" onClick={emojiToolHandler}> <i className="fa-regular fa-face-smile "></i></div> 
-                <div className="write-tool" onClick={textToolHandler}><i className="fa-solid fa-text-height"></i></div>
+                <div className="emoji-tool" onClick={scale? emojiToolHandler : null}> <i className="fa-regular fa-face-smile "></i></div> 
+                <input className="color-input" type="color" style={{pointerEvents:`${scale? "auto":"none"}`}} onChange={colorHandler}/>
+                <div className="write-tool" onClick={scale?textToolHandler:null}><i className="fa-solid fa-text-height"></i></div>
+                <div className="minimize" onClick={minimizeHandler}><i className="fa-solid fa-down-left-and-up-right-to-center" style={{fontSize:`${scale? "35px":"65px"}`,color:`${scale? "gray":"tomato"}`}}></i></div>
               </>:<>
-              {emojiToll&& <>
-            <div className="emoji-container">{EmojiArr.map((emoji)=>{
+              {emojiToll &&  <>
+            <div className="emoji-container">{EmojiArr.map((emoji,i)=>{
               return(
-                <div onClick={emojiHandler} className={emoji} >{emoji}</div>
+                <div key={i} onClick={emojiHandler } className={emoji} >{emoji}</div>
                 )
               })}</div>
               <div className="go-back" onClick={goBackHandler}><i className="fa-solid fa-arrow-left"></i></div></>}
               {textToll && <>
-                {TextSizeArr.map((size)=>{
+                {TextSizeArr.map((size,i)=>{
                   return(
-                    <div className="text-container"><i style={{fontSize:size}} className="fa-solid fa-text-height"></i></div>
+                    <div key={i}  onClick={sizeHandler}><i id={size} style={{fontSize:size}} className="fa-solid fa-text-height"></i></div>
                   )
                 })}  <div className="go-back" onClick={goBackHandler}><i className="fa-solid fa-arrow-left"></i></div>
               </>}
